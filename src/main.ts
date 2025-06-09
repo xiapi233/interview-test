2ee9bb3cc6b89007f7d9e731e1b3048cafbaca26
shark <abcliudada@gmail.com>
2025-06-09 17:51:56 +0800

    feat: change router

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import 'github-markdown-css/github-markdown.css'
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHashHistory(),
 	routes: [
 		{ path: '/test-count-down-button', component: () => import('./CountdownButton/README.mdx') },
 		{ path: '/test-pub-sub', component: () => import('./PubSub/README.mdx') },
